fb2dcd09b2cbf5fec6b815ab2dc26a017d684182
ponchis001 <145745509+ponchis001@users.noreply.github.com>
2023-09-22 02:00:46 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola cariño, esta flor amarilla es para ti:</h1>
+      <span role="img" aria-label="flor">🌼</span>
+    </div>
+  );
+};
+
+export default App;
